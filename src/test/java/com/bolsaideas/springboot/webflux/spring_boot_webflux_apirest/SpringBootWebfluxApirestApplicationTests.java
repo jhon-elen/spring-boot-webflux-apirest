269c7b24d5ec86d6334e09147d2700d94626a544
Jhon Condor <jhon_elen@outlook.es>
2024-12-13 07:27:28 -0500

    first commit

---
+++ src/test/java/com/bolsaideas/springboot/webflux/spring_boot_webflux_apirest/SpringBootWebfluxApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsaideas.springboot.webflux.spring_boot_webflux_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
