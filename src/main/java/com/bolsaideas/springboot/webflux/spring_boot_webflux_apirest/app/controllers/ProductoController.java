7912be5c87f9ad875341f12d0f6c5bc3378abca0
Jhon Condor <jhon_elen@outlook.es>
2024-12-14 16:23:13 -0500

    add upload photo, addProductWithPhoto, edit and delete methods

--- src/main/java/com/bolsaideas/springboot/webflux/spring_boot_webflux_apirest/app/controllers/ProductoController.java
+++ src/main/java/com/bolsaideas/springboot/webflux/spring_boot_webflux_apirest/app/controllers/ProductoController.java
@@ -3,14 +3,19 @@ package com.bolsaideas.springboot.webflux.spring_boot_webflux_apirest.app.contro
 import com.bolsaideas.springboot.webflux.spring_boot_webflux_apirest.app.models.documents.Producto;
 import com.bolsaideas.springboot.webflux.spring_boot_webflux_apirest.app.models.services.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.File;
 import java.net.URI;
 import java.util.Date;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/productos")
@@ -18,6 +23,44 @@ public class ProductoController {
     
     @Autowired
     private ProductoService service;
+    
+    @Value("${config.uploads.path}")
+    private String path;
+    
+    @PostMapping("/v2")
+    public Mono<ResponseEntity<Producto>> crearConFoto(Producto producto, @RequestPart FilePart file) {
+        if (producto.getCreatedAt() == null) {
+            producto.setCreatedAt(new Date());
+        }
+        
+        producto.setFoto(UUID.randomUUID().toString() + "-" + file.filename()
+            .replace(" ", "")
+            .replace(":", "")
+            .replace("\\", ""));
+        
+        return file.transferTo(new File(path + producto.getFoto()))
+            .then(service.save(producto))
+            .map(p -> ResponseEntity
+                .created(URI.create("/api/productos/".concat(p.getId())))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(p)
+            );
+    }
+    
+    @PostMapping("/upload/{id}")
+    public Mono<ResponseEntity<Producto>> upload(@PathVariable String id, @RequestPart FilePart file) {
+        return service.findById(id).flatMap(p -> {
+                p.setFoto(UUID.randomUUID().toString() + "-" + file.filename()
+                    .replace(" ", "")
+                    .replace(":", "")
+                    .replace("\\", ""));
+                
+                return file.transferTo(new File(path + p.getFoto()))
+                    .then(service.save(p));
+            })
+            .map(p -> ResponseEntity.ok(p))
+            .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
     
     @GetMapping
     public Mono<ResponseEntity<Flux<Producto>>> listar() {
@@ -46,5 +89,26 @@ public class ProductoController {
             .created(URI.create("/api/productos/".concat(p.getId())))
             .contentType(MediaType.APPLICATION_JSON)
             .body(p));
+    }
+    
+    @PutMapping("/{id}")
+    public Mono<ResponseEntity<Producto>> edita(@RequestBody Producto producto, @PathVariable String id) {
+        return service.findById(id).flatMap(p -> {
+                p.setNombre(producto.getNombre());
+                p.setPrecio(producto.getPrecio());
+                p.setCategoria(producto.getCategoria());
+                return service.save(p);
+            }).map(p -> ResponseEntity.created(URI.create("/api/productos/".concat(p.getId())))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(p))
+            .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+    
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Void>> eliminar(@PathVariable String id) {
+        return service.findById(id).flatMap(p -> {
+                return service.delete(p).then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT)));
+            })
+            .defaultIfEmpty(new ResponseEntity<Void>(HttpStatus.NOT_FOUND));
     }
 }
