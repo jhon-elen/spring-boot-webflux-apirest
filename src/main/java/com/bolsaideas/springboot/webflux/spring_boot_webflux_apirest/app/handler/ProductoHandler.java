193a943289188b7ccacc3a1b11334f02b669eda2
Jhon Condor <jhon_elen@outlook.es>
2024-12-15 22:16:42 -0500

    add createWithPhoto and create with validation

--- src/main/java/com/bolsaideas/springboot/webflux/spring_boot_webflux_apirest/app/handler/ProductoHandler.java
+++ src/main/java/com/bolsaideas/springboot/webflux/spring_boot_webflux_apirest/app/handler/ProductoHandler.java
@@ -1,16 +1,27 @@
 package com.bolsaideas.springboot.webflux.spring_boot_webflux_apirest.app.handler;
 
+import com.bolsaideas.springboot.webflux.spring_boot_webflux_apirest.app.models.documents.Categoria;
 import com.bolsaideas.springboot.webflux.spring_boot_webflux_apirest.app.models.documents.Producto;
 import com.bolsaideas.springboot.webflux.spring_boot_webflux_apirest.app.models.services.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.http.codec.multipart.FormFieldPart;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import javax.print.attribute.standard.Media;
+import java.io.File;
 import java.net.URI;
 import java.util.Date;
+import java.util.UUID;
 
 import static org.springframework.web.reactive.function.BodyInserters.fromObject;
 
@@ -19,6 +30,64 @@ public class ProductoHandler {
     
     @Autowired
     private ProductoService service;
+    
+    @Value("${config.uploads.path}")
+    private String path;
+    
+    @Autowired
+    private Validator validator;
+    
+    public Mono<ServerResponse> crearConFoto(ServerRequest request) {
+        
+        Mono<Producto> producto = request.multipartData().map(multipart -> {
+            FormFieldPart nombre = (FormFieldPart) multipart.toSingleValueMap().get("nombre");
+            FormFieldPart precio = (FormFieldPart) multipart.toSingleValueMap().get("precio");
+            FormFieldPart categoriaId = (FormFieldPart) multipart.toSingleValueMap().get("categoria.id");
+            FormFieldPart categoriaNombre = (FormFieldPart) multipart.toSingleValueMap().get("categoria.nombre");
+            
+            Categoria categoria = new Categoria(categoriaNombre.value());
+            categoria.setId(categoriaId.value());
+            return new Producto(nombre.value(), Double.parseDouble(precio.value()), categoria);
+        });
+        
+        return request.multipartData()
+            .map(multipart -> multipart.toSingleValueMap().get("file"))
+            .cast(FilePart.class)
+            .flatMap(file -> producto
+                .flatMap(p -> {
+                    p.setFoto(UUID.randomUUID().toString() + "-" + file.filename()
+                        .replace(" ", "")
+                        .replace(":", "")
+                        .replace("\\", ""));
+                    
+                    p.setCreatedAt(new Date());
+                    
+                    return file.transferTo(new File(path + p.getFoto())).then(service.save(p));
+                }))
+            .flatMap(p -> ServerResponse.created(URI.create("/api/v2/productos/".concat(p.getId())))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(fromObject(p)));
+    }
+    
+    public Mono<ServerResponse> upload(ServerRequest request) {
+        String id = request.pathVariable("id");
+        return request.multipartData()
+            .map(multipart -> multipart.toSingleValueMap().get("file"))
+            .cast(FilePart.class)
+            .flatMap(file -> service.findById(id)
+                .flatMap(p -> {
+                    p.setFoto(UUID.randomUUID().toString() + "-" + file.filename()
+                        .replace(" ", "")
+                        .replace(":", "")
+                        .replace("\\", ""));
+                    
+                    return file.transferTo(new File(path + p.getFoto())).then(service.save(p));
+                }))
+            .flatMap(p -> ServerResponse.created(URI.create("/api/v2/productos/".concat(p.getId())))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(fromObject(p)))
+            .switchIfEmpty(ServerResponse.notFound().build());
+    }
     
     public Mono<ServerResponse> listar(ServerRequest request) {
         return ServerResponse.ok()
@@ -39,15 +108,24 @@ public class ProductoHandler {
         Mono<Producto> producto = request.bodyToMono(Producto.class);
         
         return producto.flatMap(p -> {
-            if (p.getCreatedAt() == null) {
-                p.setCreatedAt(new Date());
+            Errors errors = new BeanPropertyBindingResult(p, Producto.class.getName());
+            validator.validate(p, errors);
+            
+            if (errors.hasErrors()){
+                return Flux.fromIterable(errors.getFieldErrors())
+                    .map(fieldError -> "El campo " + fieldError.getField() + " " + fieldError.getDefaultMessage())
+                    .collectList()
+                    .flatMap(list -> ServerResponse.badRequest().body(fromObject(list)));
+            } else {
+                if (p.getCreatedAt() == null) {
+                    p.setCreatedAt(new Date());
+                }
+                return service.save(p).flatMap(pdb -> ServerResponse
+                    .created(URI.create("/api/v2/productos/".concat(pdb.getId())))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(fromObject(pdb)));
             }
-            return service.save(p);
-        }).flatMap(p -> ServerResponse
-            .created(URI.create("/api/v2/productos/".concat(p.getId())))
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(fromObject(p))
-        );
+        });
     }
     
     public Mono<ServerResponse> editar(ServerRequest request) {
@@ -75,6 +153,6 @@ public class ProductoHandler {
         Mono<Producto> productoDb = service.findById(id);
         
         return productoDb.flatMap(p -> service.delete(p).then(ServerResponse.noContent().build()))
-           .switchIfEmpty(ServerResponse.notFound().build());
+            .switchIfEmpty(ServerResponse.notFound().build());
     }
 }
